ac02d39d137ea35307009df1978fdd4305ddccd7
João Pedro <nmjoaopedro22@gmail.com>
2024-06-19 22:29:04 -0300

    feat: implementa comentários em trechos

--- app/reader/_components/selection-menu.tsx
+++ app/reader/_components/selection-menu.tsx
@@ -4,10 +4,10 @@ import clsx from "clsx";
 import { memo, useCallback, useEffect, useState } from "react";
 
 import { api } from "@/trpc/react";
-import { ActionIcon, Tooltip, Transition } from "@mantine/core";
+import { ActionIcon, Button, Modal, Stack, Text, Textarea, Tooltip, Transition } from "@mantine/core";
 import { useClickOutside, useDebouncedCallback, useDisclosure } from "@mantine/hooks";
 import { notifications } from "@mantine/notifications";
-import { IconCopy, IconHighlight, IconMessagePlus } from "@tabler/icons-react";
+import { IconCopy, IconHighlight, IconMessagePlus, IconSend } from "@tabler/icons-react";
 
 import { useSettings } from "./settings-context";
 
@@ -18,18 +18,22 @@ export const SelectionMenu: React.FC<React.PropsWithChildren<SelectionMenuProps>
 }) {
 	const { currentPage, fileId } = useSettings();
 	const createHighlight = api.fileAnnotations.highlight.useMutation();
+	const createAnnotation = api.fileAnnotations.comment.useMutation();
 
 	const [open, handlers] = useDisclosure(false);
+	const [modalOpen, modalHandlers] = useDisclosure(false);
+	const [modalAnnotation, setModalAnnotation] = useState("");
+
 	const menuRef = useClickOutside(() => close());
 	const [selection, setSelection] = useState("");
 	const [location, setLocation] = useState<DOMRect | undefined>(undefined);
 
 	const close = useCallback(() => {
-		setSelection("");
 		handlers.close();
 	}, [handlers]);
 
 	const handleSelection = useDebouncedCallback(() => {
+		setSelection("");
 		const selection = window.getSelection();
 
 		let parent = selection?.anchorNode?.parentElement;
@@ -52,85 +56,140 @@ export const SelectionMenu: React.FC<React.PropsWithChildren<SelectionMenuProps>
 	}, [close, handleSelection, handlers]);
 
 	return (
-		<div id="select-menu-bounds">
-			<Transition
-				mounted={open}
-				transition="pop"
-				duration={400}
-				timingFunction="ease"
+		<>
+			<Modal
+				opened={modalOpen}
+				onClose={modalHandlers.close}
+				title="Adicionar anotação"
+				centered
 			>
-				{styles => (
-					<div
-						ref={menuRef}
-						className={clsx(
-							"absolute z-10 flex items-center justify-center gap-2 rounded-md bg-main-foreground p-2 shadow-md",
-							styles,
-						)}
-						style={{
-							top: (location?.bottom || 0) + 10,
-							left: (location?.left || 0) + (location?.width || 0) / 2 - 55,
-						}}
+				<Stack gap={10}>
+					<Text
+						size="sm"
+						className="w-full truncate"
 					>
-						<Tooltip
-							label="Marcar trecho"
-							offset={15}
-							withArrow
-						>
-							<ActionIcon
-								variant="light"
-								onClick={() => {
-									createHighlight.mutate(
-										{ fileId, page: currentPage, text: selection },
-										{
-											onError: error => {
-												notifications.show({
-													title: "Erro ao marcar trecho",
-													message: error.message || "Houve um erro desconhecido.",
-													color: "red",
-												});
-											},
+						Texto selecionado: {selection}
+					</Text>
+					<Textarea
+						label="Escreva sua anotação:"
+						placeholder="Digite aqui"
+						value={modalAnnotation}
+						onChange={event => setModalAnnotation(event.currentTarget.value)}
+					/>
+					<div className="flex w-full justify-end">
+						<Button
+							variant="light"
+							size="xs"
+							rightSection={<IconSend size={18} />}
+							onClick={() => {
+								createAnnotation.mutate(
+									{
+										fileId,
+										page: currentPage,
+										text: selection,
+										comment: modalAnnotation,
+									},
+									{
+										onError: error => {
+											notifications.show({
+												title: "Erro ao anotar trecho",
+												message: error.message || "Houve um erro desconhecido.",
+												color: "red",
+											});
 										},
-									);
-									close();
-								}}
-							>
-								<IconHighlight size={18} />
-							</ActionIcon>
-						</Tooltip>
-						<Tooltip
-							label="Adicionar anotação"
-							offset={15}
-							withArrow
+									},
+								);
+								close();
+								modalHandlers.close();
+							}}
 						>
-							<ActionIcon
-								variant="light"
-								onClick={() => {
-									close();
-								}}
-							>
-								<IconMessagePlus size={18} />
-							</ActionIcon>
-						</Tooltip>
-						<Tooltip
-							label="Copiar trecho"
-							offset={15}
-							withArrow
-						>
-							<ActionIcon
-								variant="light"
-								onClick={() => {
-									// setClipboard(selectedText);
-									close();
-								}}
-							>
-								<IconCopy size={18} />
-							</ActionIcon>
-						</Tooltip>
+							Adicionar
+						</Button>
 					</div>
-				)}
-			</Transition>
+				</Stack>
+			</Modal>
 
-			{children}
-		</div>
+			<div id="select-menu-bounds">
+				<Transition
+					mounted={open}
+					transition="pop"
+					duration={400}
+					timingFunction="ease"
+				>
+					{styles => (
+						<div
+							ref={menuRef}
+							className={clsx(
+								"absolute z-10 flex items-center justify-center gap-2 rounded-md bg-main-foreground p-2 shadow-md",
+								styles,
+							)}
+							style={{
+								top: (location?.bottom || 0) + 10,
+								left: (location?.left || 0) + (location?.width || 0) / 2 - 55,
+							}}
+						>
+							<Tooltip
+								label="Marcar trecho"
+								offset={15}
+								withArrow
+							>
+								<ActionIcon
+									variant="light"
+									onClick={() => {
+										createHighlight.mutate(
+											{ fileId, page: currentPage, text: selection },
+											{
+												onError: error => {
+													notifications.show({
+														title: "Erro ao marcar trecho",
+														message: error.message || "Houve um erro desconhecido.",
+														color: "red",
+													});
+												},
+											},
+										);
+										close();
+									}}
+								>
+									<IconHighlight size={18} />
+								</ActionIcon>
+							</Tooltip>
+							<Tooltip
+								label="Adicionar anotação"
+								offset={15}
+								withArrow
+							>
+								<ActionIcon
+									variant="light"
+									onClick={() => {
+										modalHandlers.open();
+										close();
+									}}
+								>
+									<IconMessagePlus size={18} />
+								</ActionIcon>
+							</Tooltip>
+							<Tooltip
+								label="Copiar trecho"
+								offset={15}
+								withArrow
+							>
+								<ActionIcon
+									variant="light"
+									onClick={() => {
+										// setClipboard(selectedText);
+										close();
+									}}
+								>
+									<IconCopy size={18} />
+								</ActionIcon>
+							</Tooltip>
+						</div>
+					)}
+				</Transition>
+
+				{children}
+			</div>
+		</>
 	);
 });
