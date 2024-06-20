1226adec341d734bd0b53e966ed5ddcfce7a7574
João Pedro <nmjoaopedro22@gmail.com>
2024-06-20 14:22:23 -0300

    test: continua testes de variáveis de ambiente

--- lib/url.ts
+++ lib/url.ts
@@ -1,15 +1,14 @@
-import { publicConfig } from "@/config/public";
-
 export const getURL = (): string => {
-	let url = publicConfig.NEXT_PUBLIC_WEBSERVER_URL || "https://shelfbooks.club";
+	let url = process.env.NEXT_PUBLIC_WEBSERVER_URL || "https://shelfbooks.club";
 
 	if (process.env.NODE_ENV !== "production") {
 		url = "http://localhost:3000";
 	}
 
-	if (["preview"].includes(publicConfig.NEXT_PUBLIC_VERCEL_ENV || "")) {
-		url = `https://${publicConfig.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
+	if (["preview"].includes(process.env.NEXT_PUBLIC_VERCEL_ENV || "")) {
+		url = `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
 	}
 
+	console.log(url, process.env);
 	return url;
 };
