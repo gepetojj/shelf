62676e093ff3f030edf28c32ab6f164279b030da
João Pedro <nmjoaopedro22@gmail.com>
2024-06-21 12:08:09 -0300

    adiciona fallbacks de erro

---
+++ app/(root)/(main)/error.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { AppHeader } from "@/components/ui/app-header";
+import { Button } from "@mantine/core";
+
+import Layout from "../layout";
+import { ContextProvider } from "./_components/context";
+import { Filters } from "./_components/layout/filters";
+import { Search } from "./_components/layout/search";
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+	useEffect(() => {
+		console.error(error);
+	}, [error]);
+
+	return (
+		<ContextProvider
+			value={{
+				isDrawerOpen: false,
+				discipline: "",
+				topic: "",
+				searchResults: [],
+			}}
+		>
+			<Layout>
+				<>
+					<AppHeader />
+					<Search />
+					<section className="flex h-full w-full flex-col gap-2 overflow-y-auto px-4 py-7 home-break-mobile:px-12">
+						<div className="h-52 w-full animate-pulse rounded-xl bg-main-foreground" />
+						<div className="w-full rounded-xl border border-red-500/80 bg-red-500/50">
+							<span>Houve um erro inesperado. Recarregue a página e tente novamente.</span>
+						</div>
+						<Button onClick={reset}>Tentar novamente</Button>
+					</section>
+				</>
+				<Filters />
+			</Layout>
+		</ContextProvider>
+	);
+}
---
+++ app/(root)/error.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { AppHeader } from "@/components/ui/app-header";
+import { Layout } from "@/components/ui/layout";
+import { Button } from "@mantine/core";
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+	useEffect(() => {
+		console.error(error);
+	}, [error]);
+
+	return (
+		<Layout>
+			<>
+				<AppHeader />
+				<section className="flex h-full w-full flex-col gap-2 overflow-y-auto px-4 py-7 home-break-mobile:px-12">
+					<div className="h-52 w-full animate-pulse rounded-xl bg-main-foreground" />
+					<div className="w-full rounded-xl border border-red-500/80 bg-red-500/50">
+						<span>Houve um erro inesperado. Recarregue a página e tente novamente.</span>
+					</div>
+					<Button onClick={reset}>Tentar novamente</Button>
+				</section>
+			</>
+			<></>
+		</Layout>
+	);
+}
