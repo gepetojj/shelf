bb21af94e3cc00dfd84025c40c6709dc8254ec12
João Pedro <nmjoaopedro22@gmail.com>
2024-06-28 11:29:36 -0300

    feat: adiciona termos de serviço

---
+++ app/(root)/terms/tos/page.tsx
@@ -0,0 +1,475 @@
+import { AppHeader } from "@/components/ui/app-header";
+import { Layout } from "@/components/ui/layout";
+
+export default async function Page() {
+	return (
+		<>
+			<style>{`body { scroll-behavior: smooth; }`}</style>
+
+			<Layout>
+				<>
+					<AppHeader />
+					<section className="flex h-full w-full flex-col gap-10 px-4 py-7 home-break-mobile:px-12">
+						<div className="flex flex-col gap-1">
+							<h1 className="text-2xl font-bold">Termos e condições gerais de uso</h1>
+							<h2 className="text-xs font-light italic">Válido a partir de 28 de Junho de 2024</h2>
+						</div>
+						<div
+							id="section-0"
+							className="flex flex-col gap-3 text-neutral-200"
+						>
+							<p className="text-justify">
+								Termos e condições gerais de uso (e de compra e venda) do OU site Shelfbooks OU
+								aplicativo Shelfbooks.
+							</p>
+							<p>
+								Os serviços do Shelfbooks são fornecidos pela pessoa jurídica OU física com a seguinte
+								Razão Social/Nome: 52.717.128 Henri Fagilde Paixao, com nome fantasia Cosmix, inscrito
+								no CNPJ/CPF sob o nº 52717128000174, titular da propriedade intelectual sobre software,
+								website, aplicativos, conteúdos e demais ativos relacionados à plataforma Shelfbooks.
+							</p>
+						</div>
+
+						<div
+							id="section-1"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-1"
+								className="text-xl font-medium text-neutral-100"
+							>
+								1. Do objeto
+							</a>
+							<p>
+								A plataforma visa licenciar o uso de seu software, website, aplicativos e demais ativos
+								de propriedade intelectual, fornecendo ferramentas para auxiliar e dinamizar o dia a dia
+								dos seus usuários.
+							</p>
+							<p>
+								A plataforma caracteriza-se pela prestação do seguinte serviço: Leitura e
+								compartilhamento de arquivos PDF.
+							</p>
+							<p>
+								A plataforma realiza a venda à distância por meio eletrônico dos seguintes produtos ou
+								serviços: Assinatura.
+							</p>
+						</div>
+
+						<div
+							id="section-2"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-2"
+								className="text-xl font-medium text-neutral-100"
+							>
+								2. Da aceitação
+							</a>
+							<p>
+								O presente Termo estabelece obrigações contratadas de livre e espontânea vontade, por
+								tempo indeterminado, entre a plataforma e as pessoas físicas ou jurídicas, usuárias do
+								OU site OU aplicativo.
+							</p>
+							<p>
+								Ao utilizar a plataforma o usuário aceita integralmente as presentes normas e
+								compromete-se a observá-las, sob o risco de aplicação das penalidades cabíveis.
+							</p>
+							<p>
+								A aceitação do presente instrumento é imprescindível para o acesso e para a utilização
+								de quaisquer serviços fornecidos pela empresa. Caso não concorde com as disposições
+								deste instrumento, o usuário não deve utilizá-los.
+							</p>
+						</div>
+
+						<div
+							id="section-3"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-3"
+								className="text-xl font-medium text-neutral-100"
+							>
+								3. Do acesso dos usuários
+							</a>
+							<p>
+								Serão utilizadas todas as soluções técnicas à disposição do responsável pela plataforma
+								para permitir o acesso ao serviço 24 (vinte e quatro) horas por dia, 7 (sete) dias por
+								semana. No entanto, a navegação na plataforma ou em alguma de suas páginas poderá ser
+								interrompida, limitada ou suspensa para atualizações, modificações ou qualquer ação
+								necessária ao seu bom funcionamento.
+							</p>
+						</div>
+
+						<div
+							id="section-4"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-4"
+								className="text-xl font-medium text-neutral-100"
+							>
+								4. Do cadastro
+							</a>
+							<p>
+								O acesso às funcionalidades da plataforma exigirá a realização de um cadastro prévio e,
+								a depender dos serviços ou produtos escolhidos, o pagamento de determinado valor.
+							</p>
+							<p>
+								Ao se cadastrar o usuário deverá informar dados completos, recentes e válidos, sendo de
+								sua exclusiva responsabilidade manter referidos dados atualizados, bem como o usuário se
+								compromete com a veracidade dos dados fornecidos.
+							</p>
+							<p>
+								O usuário se compromete a não informar seus dados cadastrais e/ou de acesso à plataforma
+								a terceiros, responsabilizando-se integralmente pelo uso que deles seja feito.
+							</p>
+							<p>
+								Menores de 18 anos e aqueles que não possuírem plena capacidade civil deverão obter
+								previamente o consentimento expresso de seus responsáveis legais para utilização da
+								plataforma e dos serviços ou produtos, sendo de responsabilidade exclusiva dos mesmos o
+								eventual acesso por menores de idade e por aqueles que não possuem plena capacidade
+								civil sem a prévia autorização.
+							</p>
+							<p>
+								Mediante a realização do cadastro o usuário declara e garante expressamente ser
+								plenamente capaz, podendo exercer e usufruir livremente dos serviços e produtos.
+							</p>
+							<p>
+								O usuário deverá fornecer um endereço de e-mail válido, através do qual o site realizará
+								todas comunicações necessárias.
+							</p>
+							<p>
+								Após a confirmação do cadastro, o usuário possuirá um login e uma senha pessoal, a qual
+								assegura ao usuário o acesso individual à mesma. Desta forma, compete ao usuário
+								exclusivamente a manutenção de referida senha de maneira confidencial e segura, evitando
+								o acesso indevido às informações pessoais.
+							</p>
+							<p>
+								Toda e qualquer atividade realizada com o uso da senha será de responsabilidade do
+								usuário, que deverá informar prontamente a plataforma em caso de uso indevido da
+								respectiva senha.
+							</p>
+							<p>
+								Não será permitido ceder, vender, alugar ou transferir, de qualquer forma, a conta, que
+								é pessoal e intransferível.
+							</p>
+							<p>
+								Caberá ao usuário assegurar que o seu equipamento seja compatível com as características
+								técnicas que viabilize a utilização da plataforma e dos serviços ou produtos.
+							</p>
+							<p>
+								O usuário poderá, a qualquer tempo, requerer o cancelamento de seu cadastro junto ao
+								site OU aplicativo. O seu descadastramento será realizado o mais rapidamente possível,
+								desde que não sejam verificados débitos em aberto.
+							</p>
+							<p>
+								O usuário, ao aceitar os Termos e Política de Privacidade, autoriza expressamente a
+								plataforma a coletar, usar, armazenar, tratar, ceder ou utilizar as informações
+								derivadas do uso dos serviços, do site e quaisquer plataformas, incluindo todas as
+								informações preenchidas pelo usuário no momento em que realizar ou atualizar seu
+								cadastro, além de outras expressamente descritas na Política de Privacidade que deverá
+								ser autorizada pelo usuário.
+							</p>
+						</div>
+
+						<div
+							id="section-5"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-5"
+								className="text-xl font-medium text-neutral-100"
+							>
+								5. Dos serviços ou produtos
+							</a>
+							<p>
+								A plataforma poderá disponibilizar para o usuário um conjunto específico de
+								funcionalidades e ferramentas para otimizar o uso dos serviços e produtos.
+							</p>
+							<p>
+								Na plataforma os serviços ou produtos oferecidos estão descritos e apresentados com o
+								maior grau de exatidão, contendo informações sobre suas características, qualidades,
+								quantidades, composição, preço, garantia, prazos de validade e origem, entre outros
+								dados, bem como sobre os riscos que apresentam à saúde e segurança do usuário.
+							</p>
+							<p>
+								Antes de finalizar a compra sobre determinado produto ou serviço, o usuário deverá se
+								informar sobre as suas especificações e sobre a sua destinação.
+							</p>
+						</div>
+
+						<div
+							id="section-6"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-6"
+								className="text-xl font-medium text-neutral-100"
+							>
+								6. Dos preços
+							</a>
+							<p>
+								A plataforma se reserva no direito de reajustar unilateralmente, a qualquer tempo, os
+								valores dos serviços ou produtos sem consulta ou anuência prévia do usuário.
+							</p>
+							<p>Os valores aplicados são aqueles que estão em vigor no momento do pedido.</p>
+							<p>
+								Os preços são indicados em reais e não incluem as taxas de entrega, as quais são
+								especificadas à parte e são informadas ao usuário antes da finalização do pedido.
+							</p>
+							<p>
+								Na contratação de determinado serviço ou produto, a plataforma poderá solicitar as
+								informações financeiras do usuário, como CPF, endereço de cobrança e dados de cartões.
+								Ao inserir referidos dados o usuário concorda que sejam cobrados, de acordo com a forma
+								de pagamento que venha a ser escolhida, os preços então vigentes e informados quando da
+								contratação. Referidos dados financeiros poderão ser armazenados para facilitar acessos
+								e contratações futuras.
+							</p>
+							<p>
+								A contratação dos serviços será renovada automaticamente pela plataforma,
+								independentemente de comunicação ao usuário, mediante cobrança periódica da mesma forma
+								de pagamento indicada pelo usuário quando da contratação do serviço.
+							</p>
+						</div>
+
+						<div
+							id="section-7"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-7"
+								className="text-xl font-medium text-neutral-100"
+							>
+								7. Do cancelamento
+							</a>
+							<p>
+								O usuário poderá cancelar a contratação dos serviços de acordo com os termos que forem
+								definidos no momento de sua contratação. Ainda, o usuário também poderá cancelar os
+								serviços em até 7 (sete) dias após a contratação, mediante contato com o email de
+								suporte de acordo com o Código de Defesa do Consumidor (Lei no. 8.078/90).
+							</p>
+							<p>O serviço poderá ser cancelado por:</p>
+							<ul className="flex flex-col gap-2 px-8 text-justify">
+								<li>
+									a) parte do usuário: nessas condições os serviços somente cessarão quando concluído
+									o ciclo vigente ao tempo do cancelamento;
+								</li>
+								<li>b) violação dos Termos de Uso: os serviços serão cessados imediatamente.</li>
+							</ul>
+						</div>
+
+						<div
+							id="section-8"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-8"
+								className="text-xl font-medium text-neutral-100"
+							>
+								8. Do suporte
+							</a>
+							<p>
+								Em caso de qualquer dúvida, sugestão ou problema com a utilização da plataforma, o
+								usuário poderá entrar em contato com o suporte, através do email shelf@shelfbooks.club.
+							</p>
+							<p>
+								Estes serviços de atendimento ao usuário estarão disponíveis nos seguintes dias e
+								horários: 10:00 - 16:00.
+							</p>
+						</div>
+
+						<div
+							id="section-9"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-9"
+								className="text-xl font-medium text-neutral-100"
+							>
+								9. Das responsabilidades
+							</a>
+							<p>É de responsabilidade do usuário:</p>
+							<ul className="flex flex-col gap-2 px-8 text-justify">
+								<li>a) defeitos ou vícios técnicos originados no próprio sistema do usuário;</li>
+								<li>
+									b) a correta utilização da plataforma, dos serviços ou produtos oferecidos, prezando
+									pela boa convivência, pelo respeito e cordialidade entre os usuários;
+								</li>
+								<li>
+									c) pelo cumprimento e respeito ao conjunto de regras disposto nesse Termo de
+									Condições Geral de Uso, na respectiva Política de Privacidade e na legislação
+									nacional e internacional;
+								</li>
+								<li>d) pela proteção aos dados de acesso à sua conta/perfil (login e senha).</li>
+							</ul>
+							<p>É de responsabilidade da plataforma Shelfbooks:</p>
+							<ul className="flex flex-col gap-2 px-8 text-justify">
+								<li>a) indicar as características do serviço ou produto;</li>
+								<li>
+									b) os defeitos e vícios encontrados no serviço ou produto oferecido desde que lhe
+									tenha dado causa;
+								</li>
+								<li>
+									c) as informações que foram por ele divulgadas, sendo que os comentários ou
+									informações divulgadas por usuários são de inteira responsabilidade dos próprios
+									usuários;
+								</li>
+							</ul>
+							<p>
+								A plataforma não se responsabiliza por links externos contidos em seu sistema que possam
+								redirecionar o usuário ao ambiente externo a sua rede.
+							</p>
+							<p>
+								Não poderão ser incluídos links externos ou páginas que sirvam para fins comerciais ou
+								publicitários ou quaisquer informações ilícitas, violentas, polêmicas, pornográficas,
+								xenofóbicas, discriminatórias ou ofensivas.
+							</p>
+						</div>
+
+						<div
+							id="section-10"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-10"
+								className="text-xl font-medium text-neutral-100"
+							>
+								10. Dos direitos autorais
+							</a>
+							<p>
+								O presente Termo de Uso concede aos usuários uma licença não exclusiva, não transferível
+								e não sublicenciável, para acessar e fazer uso da plataforma e dos serviços e produtos
+								por ela disponibilizados.
+							</p>
+							<p>
+								A estrutura do site ou aplicativo, as marcas, logotipos, nomes comerciais, layouts,
+								gráficos e design de interface, imagens, ilustrações, fotografias, apresentações,
+								vídeos, conteúdos escritos e de som e áudio, programas de computador, banco de dados,
+								arquivos de transmissão e quaisquer outras informações e direitos de propriedade
+								intelectual da razão social, observados os termos da Lei da Propriedade Industrial (Lei
+								nº 9.279/96),
+							</p>
+							<p>
+								Lei de Direitos Autorais (Lei nº 9.610/98) e Lei do Software (Lei nº 9.609/98), estão
+								devidamente reservados.
+							</p>
+							<p>
+								Este Termos de Uso não cede ou transfere ao usuário qualquer direito, de modo que o
+								acesso não gera qualquer direito de propriedade intelectual ao usuário, exceto pela
+								licença limitada ora concedida.
+							</p>
+							<p>
+								O uso da plataforma pelo usuário é pessoal, individual e intransferível, sendo vedado
+								qualquer uso não autorizado, comercial ou não-comercial. Tais usos consistirão em
+								violação dos direitos de propriedade intelectual da razão social, puníveis nos termos da
+								legislação aplicável.
+							</p>
+						</div>
+
+						<div
+							id="section-11"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-11"
+								className="text-xl font-medium text-neutral-100"
+							>
+								11. Das sanções
+							</a>
+							<p>
+								Sem prejuízo das demais medidas legais cabíveis, a razão social poderá, a qualquer
+								momento, advertir, suspender ou cancelar a conta do usuário:
+							</p>
+							<ul className="flex flex-col gap-2 px-8 text-justify">
+								<li>a) que violar qualquer dispositivo do presente Termo;</li>
+								<li>b) que descumprir os seus deveres de usuário;</li>
+								<li>
+									c) que tenha qualquer comportamento fraudulento, doloso ou que ofenda a terceiros.
+								</li>
+							</ul>
+						</div>
+
+						<div
+							id="section-12"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-12"
+								className="text-xl font-medium text-neutral-100"
+							>
+								12. Da rescisão
+							</a>
+							<p>
+								A não observância das obrigações pactuadas neste Termo de Uso ou da legislação aplicável
+								poderá, sem prévio aviso, ensejar a imediata rescisão unilateral por parte da razão
+								social o bloqueio de todos os serviços prestados ao usuário.
+							</p>
+						</div>
+
+						<div
+							id="section-13"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-13"
+								className="text-xl font-medium text-neutral-100"
+							>
+								13. Das alterações
+							</a>
+							<p>
+								Os itens descritos no presente instrumento poderão sofrer alterações, unilateralmente e
+								a qualquer tempo, por parte de Shelfbooks, para adequar ou modificar os serviços, bem
+								como para atender novas exigências legais. As alterações serão veiculadas OU pelo site
+								shelbooks.club OU pelo aplicativo e o usuário poderá optar por aceitar o novo conteúdo
+								ou por cancelar o uso dos serviços, caso seja assinante de algum serviço.
+							</p>
+							<p>
+								Os serviços oferecidos podem, a qualquer tempo e unilateralmente, e sem qualquer aviso
+								prévio, ser deixados de fornecer, alterados em suas características, bem como
+								restringido para o uso ou acesso.
+							</p>
+						</div>
+
+						<div
+							id="section-14"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-14"
+								className="text-xl font-medium text-neutral-100"
+							>
+								14. Da política de privacidade
+							</a>
+							<p>
+								Além do presente Termo, o usuário deverá consentir com as disposições contidas na
+								respectiva Política de Privacidade a ser apresentada a todos os interessados dentro da
+								interface da plataforma.
+							</p>
+						</div>
+
+						<div
+							id="section-15"
+							className="flex flex-col gap-3 text-justify text-neutral-200"
+						>
+							<a
+								href="#section-15"
+								className="text-xl font-medium text-neutral-100"
+							>
+								15. Do foro
+							</a>
+							<p>
+								Para a solução de controvérsias decorrentes do presente instrumento será aplicado
+								integralmente o Direito brasileiro.
+							</p>
+							<p>
+								Os eventuais litígios deverão ser apresentados no foro da comarca em que se encontra a
+								sede da empresa.
+							</p>
+						</div>
+					</section>
+				</>
+				<></>
+			</Layout>
+		</>
+	);
+}
