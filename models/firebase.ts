b8b7cc3a9b121b47c647869dfc13592d1430cbe8
João Pedro <nmjoaopedro22@gmail.com>
2024-07-01 18:17:34 -0300

    feat: adiciona regra no storage para deletar arquivos temporários

--- models/firebase.ts
+++ models/firebase.ts
@@ -15,4 +15,24 @@ if (!firebase.apps.length) {
 	});
 }
 
-export const storage = firebase.storage().bucket();
+const storage = firebase.storage().bucket();
+
+if (process.env.NODE_ENV === "production") {
+	try {
+		storage.addLifecycleRule({
+			action: {
+				type: "Delete",
+			},
+			condition: {
+				age: 1,
+				matchesPrefix: ["chunk_uploads"],
+			},
+		});
+	} catch (err: any) {
+		console.error("Failed to set lifecycle rule", {
+			err: err.message || err.stack || err,
+		});
+	}
+}
+
+export { storage };
