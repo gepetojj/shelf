6341ad656bdfff6b58cfa32ef17aaf1a2362c8de
João Pedro <nmjoaopedro22@gmail.com>
2024-07-06 10:44:08 -0300

    fix: corrige routeMatcher no clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
 	"/dashboard(.*)",
-	"/notifications/(.*)",
-	"/streak/(.*)",
-	"/shelf/(.*)",
+	"/notifications(.*)",
+	"/streak(.*)",
+	"/shelf(.*)",
 	"/book/new",
 ]);
 
